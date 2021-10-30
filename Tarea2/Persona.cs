ce121b9c924bb5c811885064c893526f1a8bd540
Sebaxd12 <sebastianmelinao762@gmail.com>
2021-10-30 12:55:59 -0300

    Agregar archivos de proyecto.

---
+++ Tarea2/Persona.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tarea2
+{
+    class Persona
+    {
+        private string rut;
+        private string nombre;
+        private string apellido_pa;
+        private string apellido_ma;
+        private string edad;
+        private string correo;
+        private string fono;
+        public Persona()
+        {
+
+        }
+       
+
+        public string Rut { get => rut; set => rut = value; }
+        public string Nombre { get => nombre; set => nombre = value; }
+        public string Apellido_pa { get => apellido_pa; set => apellido_pa = value; }
+        public string Apellido_ma { get => apellido_ma; set => apellido_ma = value; }
+        public string Edad { get => edad; set => edad = value; }
+        public string Correo { get => correo; set => correo = value; }
+        public string Fono { get => fono; set => fono = value; }
+        public string GetRut()
+        {
+            return rut;
+        }
+        public void SetRut(string nuevoRut)
+        {
+            rut = nuevoRut;
+        }
+        
+    }
+}
---
+++ Tarea2/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+namespace Tarea2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            bool confirmacion = true;
+
+            if (confirmacion = true) {
+
+                Console.WriteLine("                           FORMULARIO DE CONTACTO");
+                Console.WriteLine("Ingrese la cantidad de persona que quiere registrar:");
+
+                int numerodedatos = Convert.ToInt32(Console.ReadLine());
+                Persona[] ListaPersona = new Persona[numerodedatos];
+
+                Persona p4 = new Persona();
+
+                ListaPersona[0] = p4;
+
+              
+
+                for (int i=0; i< numerodedatos; i++)
+
+                {
+                    Console.WriteLine("Ingrese su Rut:");
+                    p4.Rut = Convert.ToString(Console.ReadLine());
+                    Console.WriteLine("Ingrese su Nombre:");
+                    p4.Nombre = Convert.ToString(Console.ReadLine());
+                    Console.WriteLine("Ingrese su Apellido Paterno:");
+                    p4.Apellido_pa = Convert.ToString(Console.ReadLine());
+                    Console.WriteLine("Ingrese su Apellido Materno:");
+                    p4.Apellido_ma = Convert.ToString(Console.ReadLine());
+                    Console.WriteLine("Ingrese su Edad:");
+                    p4.Edad = Convert.ToString(Console.ReadLine());
+                    Console.WriteLine("Ingrese su Correo:");
+                    p4.Correo = Convert.ToString(Console.ReadLine());
+                    Console.WriteLine("Ingrese su Fono:");
+                    p4.Fono = Convert.ToString(Console.ReadLine());
+
+                    
+                    ListaPersona[i] = p4;
+                }
+                foreach (Persona datoss in ListaPersona)
+                {
+                    Console.WriteLine("LOS DATOS DE LAS PERSONAS INGRESADAS SON:");
+                    Console.WriteLine("                " + p4.Rut);
+                    Console.WriteLine("                " + p4.Nombre);
+                    Console.WriteLine("                " + p4.Apellido_pa);
+                    Console.WriteLine("                " + p4.Apellido_ma);
+                    Console.WriteLine("                " + p4.Edad);
+                    Console.WriteLine("                " + p4.Correo);
+                    Console.WriteLine("                " + p4.Fono);
+
+                }
+
+
+
+            }
+
+        }
+    }
+}
